690eda3a9c3a24106ef4594ba3957385bf771479
Vladislav <nahornyi.vlad@gmail.com>
2021-03-25 17:03:11 +0200

    Fix delete alias

--- src/main/java/edu/kpi/testcourse/storage/UrlRepositoryFakeImpl.java
+++ src/main/java/edu/kpi/testcourse/storage/UrlRepositoryFakeImpl.java
@@ -4,12 +4,14 @@ import edu.kpi.testcourse.entities.UrlAlias;
 import java.util.HashMap;
 import java.util.List;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 
 /**
  * An in-memory fake implementation of {@link UrlRepository}.
  */
 public class UrlRepositoryFakeImpl implements UrlRepository {
   private final HashMap<String, UrlAlias> aliases = new HashMap<>();
+
 
   @Override
   public void createUrlAlias(UrlAlias urlAlias) {
@@ -20,16 +22,24 @@ public class UrlRepositoryFakeImpl implements UrlRepository {
     aliases.put(urlAlias.alias(), urlAlias);
   }
 
+
   @Override
   public @Nullable UrlAlias findUrlAlias(String alias) {
     return aliases.get(alias);
   }
 
+
   @Override
-  public void deleteUrlAlias(String email, String alias) {
-    // TODO: We should implement it
-    throw new UnsupportedOperationException();
+  public void deleteUrlAlias (String email, String alias) throws PermissionDenied{
+    UrlAlias savedAlias = aliases.get(alias);
+    if (savedAlias != null && savedAlias.email().equals(email)) {
+      aliases.remove(alias);
+    }
+    else {
+      throw new PermissionDenied();
+    }
   }
+
 
   @Override
   public List<UrlAlias> getAllAliasesForUser(String userEmail) {
